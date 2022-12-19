c1e3452726dd15ccd543052d5304af91fdda1a72
hector.gv00 <hector.gv00@gmail.com>
2022-12-19 19:15:17 +0100

    [structure]: Created Home Component. Routed it and added a sample view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './modules/home/home-component/home-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // Route /
+    path:"",
+    component:HomeComponentComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home/home.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    HomeComponentComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
